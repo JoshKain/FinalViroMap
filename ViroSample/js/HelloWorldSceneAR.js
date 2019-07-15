223f63b70c8f4c174701d6cb8ed690958a166b3f
Kelly Davidson <kellyadavidsonuk@gmail.com>
2019-07-15 10:43:08 +0100

    added target for AR and image to render

--- ViroSample/js/HelloWorldSceneAR.js
+++ ViroSample/js/HelloWorldSceneAR.js
@@ -1,10 +1,10 @@
-'use strict';
+"use strict";
 
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import { StyleSheet, View, Button, Text } from 'react-native';
+import { StyleSheet, View, Button, Text } from "react-native";
 
-import { ViroARScene, ViroText, ViroConstants } from 'react-viro';
+import { ViroARScene, ViroText, ViroConstants } from "react-viro";
 
 export default class HelloWorldSceneAR extends Component {
   constructor() {
@@ -12,7 +12,7 @@ export default class HelloWorldSceneAR extends Component {
 
     // Set initial state here
     this.state = {
-      text: 'Initializing AR...'
+      text: "Initializing AR..."
     };
 
     // bind 'this' to functions
@@ -21,23 +21,25 @@ export default class HelloWorldSceneAR extends Component {
 
   render() {
     return (
-      <>
-        <ViroARScene onTrackingUpdated={this._onInitialized}>
-          <ViroText
-            text={this.state.text}
-            scale={[0.5, 0.5, 0.5]}
-            position={[0, 0, -1]}
-            style={styles.helloWorldTextStyle}
+      <ViroARScene>
+        <ViroARImageMarker target={"targetOne"}>
+          {/* <ViroBox position={[0, 0.25, 0]} scale={[0.5, 0.5, 0.5]} /> */}
+          <ViroImage
+            height={1}
+            width={1}
+            placeholderSource={require("../images/platform.jpg")}
+            source={require("../images/platform.jpg")}
+            rotation={[-90, 0, 0]}
           />
-        </ViroARScene>
-      </>
+        </ViroARImageMarker>
+      </ViroARScene>
     );
   }
 
   _onInitialized(state, reason) {
     if (state == ViroConstants.TRACKING_NORMAL) {
       this.setState({
-        text: 'Hello Mate!'
+        text: "Hello Mate!"
       });
     } else if (state == ViroConstants.TRACKING_NONE) {
       // Handle loss of tracking
@@ -45,13 +47,21 @@ export default class HelloWorldSceneAR extends Component {
   }
 }
 
+ViroARTrackingTargets.createTargets({
+  targetOne: {
+    source: require("../images/target.jpg"),
+    orientation: "Up",
+    physicalWidth: 0.25 // real world width in meters
+  }
+});
+
 var styles = StyleSheet.create({
   helloWorldTextStyle: {
-    fontFamily: 'Arial',
+    fontFamily: "Arial",
     fontSize: 30,
-    color: '#ffffff',
-    textAlignVertical: 'center',
-    textAlign: 'center'
+    color: "#ffffff",
+    textAlignVertical: "center",
+    textAlign: "center"
   }
 });
 
