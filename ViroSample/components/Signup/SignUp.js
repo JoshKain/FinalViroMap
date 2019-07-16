c53740112d242da89ed08f834626097cea287e9f
JoshKain <JoshKain0612@gmail.com>
2019-07-16 10:35:12 +0100

    completeing SignUp form and navigation homePage and form

--- ViroSample/components/Signup/SignUp.js
+++ ViroSample/components/Signup/SignUp.js
@@ -23,15 +23,20 @@ export default class SignUp extends Component {
     return (
       <View style={styles.container}>
         <Form type={User} ref={type => (this.input = type)} />
-        <Button
-          title="Already have an account? Login!"
-          onPress={this.handleSubmit}
-        />
-
-        <Button
-          title="Sign Up!"
-          onPress={() => this.props.navigation.navigate("Login")}
-        />
+        <View style={{ marginTop: 15 }}>
+          <Button
+            title="Already have an account? Login!"
+            onPress={this.handleSubmit}
+            color="#25a7e3"
+          />
+        </View>
+        <View style={{ marginTop: 15 }}>
+          <Button
+            title="Sign Up!"
+            onPress={() => this.props.navigation.navigate("Login")}
+            color="#25a7e3"
+          />
+        </View>
       </View>
     );
   }
@@ -39,9 +44,39 @@ export default class SignUp extends Component {
 
 const styles = StyleSheet.create({
   container: {
+    flexDirection: "column",
     justifyContent: "center",
     marginTop: 50,
     padding: 20,
-    backgroundColor: "#ffffff"
+    backgroundColor: "#ffffff",
+    alignItems: "stretch"
   }
 });
+
+//Had to add this to App.js on Virosample to get navigation between screens
+
+//_getSignUp() {
+//   return (
+//     <View>
+//       <SignUp />
+//     </View>
+//   );
+// }
+
+// within the _getExperienceSelector()
+
+//  <TouchableHighlight
+// underlayColor={"#FF0000"}
+// style={localStyles.buttons}
+// onPress={this._getExperienceButtonOnPress(SIGNUP_NAVIGATOR_TYPE)}
+// >
+// <Text style={localStyles.buttonText}>SignUp</Text>
+// </TouchableHighlight>
+
+//within the class Virosample  before the render
+
+//this._getSignUp = this._getSignUp.bind(this);
+
+//above the class
+
+//var SIGNUP_NAVIGATOR_TYPE = "SIGNUP";
